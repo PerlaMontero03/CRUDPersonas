f30598755c8ad8b1be1ee00ceb0989dcc1d8a884
Perla Montero <perla_michelle2000@hotmail.com>
2022-06-30 23:20:31 -0400

    Agregar archivos de proyecto.

---
+++ WebAppPrueba/Models/PruebaContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace WebAppPrueba.Models
+{
+    public partial class PruebaContext : DbContext
+    {
+        public PruebaContext()
+        {
+        }
+
+        public PruebaContext(DbContextOptions<PruebaContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Persona> Persona { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-4QR3QKP\\SQLEXPRESS;Database=Prueba;persist security info=True;Integrated Security=SSPI;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Persona>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.FechaDeNacimiento).HasColumnType("date");
+
+                entity.Property(e => e.Nombre)
+                    .HasMaxLength(150)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
